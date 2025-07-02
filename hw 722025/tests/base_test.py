cb65c613e0d204ff633c241b8b7efa0ffb073784
MaiLinh001 <maithithuylinhcc@gmail.com>
2025-07-02 11:48:45 +0700

    Add files via upload

---
+++ hw 722025/tests/base_test.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from time import sleep
+
+class BaseTest:
+    @pytest.fixture(autouse=True)
+    def setup(self):
+        s = Service(executable_path="D:\\chromedriver-win64\\chromedriver.exe")
+        self.driver = webdriver.Chrome(service=s)
+        self.driver.maximize_window()
+        self.driver.implicitly_wait(10)
+        self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        yield
+        self.driver.quit()
+
+    
